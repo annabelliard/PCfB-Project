d7399e9cf17746dbfab6b0c2ff10e3a790a8aa83
Aoife <a.o.sullivan@student.rug.nl>
2017-01-26 17:42:00 +0100

    Updated Code 26-01-17

--- MapProject.R
+++ MapProject.R
@@ -18,51 +18,50 @@ library(ggmap)
 library (automap)
 
 # getting the map- defining the area
-mapgilbert <- get_map(location = c(Long = mean(d$Long), Lat = mean(d$Lat)), zoom = 4,
+mapMed <- get_map(location = c(Long = mean(d$Long), Lat = mean(d$Lat)), zoom = 4,
                       maptype = "satellite", scale = 2)
 
 
-# plotting the map with some points on it
-ggmap(mapgilbert) +
+# plotting the map with populations and Ho (Heterozygozity) in a gradient of low (yellow) to high (red)
+ggmap(mapMed) +
   geom_point(data = d, aes(x = Long, y = Lat, colour = Ho), size = 4, alpha=0.8, shape = 19)+ 
   scale_colour_gradient (low= "yellow", high = "red")
 
 
 
-
 #====================================================================#
 
-
-# Checkout- fields, maps & mapdata packages
-# Within those packages, checkout tps, surface, maps & points functions
-# fields package pg 41
+# Making a human impact gradient map from the fields package
 
 library (mapdata)
 library (maps)
 library (fields)
+library (png)
 
 # define ylim and xlim 
+# extending the latitude as the initial map was contorted
 xlim <- c(min(d$Long)-0.1, max(d$Long)+0.1)
-ylim <- c(min(d$Lat)-0.1, max(d$Lat)+0.1)
+ylim <- c(min(d$Lat)-12.9, max(d$Lat)+12.9)
 
 # Tps() function from fields package
-#x <- as.matrix(d$Lat, d$Long)
 # make lat and long into a matrix
+# cthreat stand for cumulative human impact
 x <- cbind(d$Long, d$Lat)
 y <- d$cthreat
 
 # use the tps function to fit the impacts
 fit<- Tps(x,y, lambda=0)
+# save map as png in home directory
+png("map.png",height=1000,width=1000)
 surface(fit, xlim=xlim, ylim=ylim, xlab= "Longitude", ylab="Latitude", 
         main= "Human impacts on allelic richness")
-#map('worldHires',  add=TRUE)# change map type here
+
+#plot the map
 map('worldHires', add=TRUE, fill=TRUE, col='white', boundary='black', lwd = 2)
 
-
-# plot points per freq of alleles per locus
-colours <- c("paleturquoise","darkslategray3","darkkhaki","thistle")
-colours1 <- colours[d$Na]
-points(d$Long, d$Lat, cex=(d$Na), pch=21, bg=colours1)
+# add data point with Ho (the higher the Ho the bigger the circle)
+points(cbind(d$Long, d$Lat), pch=16, cex=d$Ho*10, col = alpha("black",0.8)) 
+dev.off()
 
 
 
